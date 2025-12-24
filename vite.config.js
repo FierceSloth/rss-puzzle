e42ff8fee6f87c624077bdfa456bb12e8dcc70a7
Dastan Hairushev <hairushevdastan@gmail.com>
2025-12-24 18:27:15 +0500

    feat: implement Logo component and add page background

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@pages': path.resolve(__dirname, './src/pages'),
+      '@assets': path.resolve(__dirname, './src/assets'),
       '@common': path.resolve(__dirname, './src/common'),
       '@utils': path.resolve(__dirname, './src/common/utils'),
       '@constants': path.resolve(__dirname, './src/common/constants'),
