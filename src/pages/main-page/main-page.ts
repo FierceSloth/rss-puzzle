0875920e21a0d976972fb83b70b8d560c6921923
Dastan Hairushev <hairushevdastan@gmail.com>
2025-12-25 14:35:47 +0500

    feat: implement Main Page layout

--- src/pages/main-page/main-page.ts
+++ src/pages/main-page/main-page.ts
@@ -3,6 +3,8 @@ import { Component } from '@/common/base-component';
 import type { Router } from '@/router/router';
 
 import styles from './main-page.module.scss';
+import { BaseCard } from '@/components/ui/card/card';
+import { Button } from '@/components/ui/button/button';
 
 export class MainPage {
   private container: Component;
@@ -15,13 +17,31 @@ export class MainPage {
   }
 
   render(): void {
+    // ================== Header ===============
+
     const header = new Header({ className: [styles.header] });
 
-    const pageContainer = new Component({ className: 'pageContainer' }, header);
-    this.container.append(pageContainer);
-  }
+    // ================== TextContainer ===============
 
-  temporaryMethod(): Router {
-    return this.router; // ? Temporarily, so that eslint doesn't complain that the router is not being used
+    const title = new Component({ tag: 'h1', className: styles.title, text: 'Welcome back, User Name!' });
+    const text = new Component({ tag: 'p', className: styles.text, text: 'Assemble sentences, reveal masterpieces.' });
+    const textContainer = new Component({ className: styles.textContainer }, title, text);
+
+    // ================== Button ===============
+
+    const startBtn = new Button({
+      className: [styles.startBtn],
+      text: 'Start Game',
+      onClick: () => {
+        this.router.navigate('/game');
+      },
+    });
+
+    // ================== Containers ===============
+
+    const card = new BaseCard({ className: [styles.card], children: [textContainer, startBtn] });
+
+    const pageContainer = new Component({ className: ['pageContainer', styles.mainContainer] }, card);
+    this.container.appendChildren([header, pageContainer]);
   }
 }
