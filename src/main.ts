1472f29d1b66a994eafec88227d75036d88a4cd8
Dastan Hairushev <hairushevdastan@gmail.com>
2026-03-18 00:18:42 +0500

    chore: deploy rss-puzzle to github pages

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { App } from './app';
-import './styles/main.scss';
-
-alert(`Пожалуйста, если будет время и возможность, проверь мою работу ближе к четвергу, буду очень благодарен! Заранее спасибо 🙂
-  
-Please, if you have time and the opportunity, check my work closer to Thursday. I would really appreciate it. Thanks in advance 🙂`);
-
-const app = new App();
-app.start();
